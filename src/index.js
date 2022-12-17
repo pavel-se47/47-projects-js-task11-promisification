7908b93bbf4c6eb4e6476a48eed96b247030e8fb
Pavel Hubarenka <pavel-se47@mail.ru>
2022-12-18 00:45:56 +0300

    mod 11 web1 & web2

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// import taskPromis from './task-promis-11.1';
+// import taskPromis from './task-promis-11.2';
+import taskPromis from './task-promis-11.3';
---
+++ src/task-promis-11.2.js
@@ -0,0 +1,47 @@
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user
+//   );
+
+//   callback(updatedUsers);
+// };
+
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// /*
+//  * Сейчас работает так
+//  */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve, reject) => {
+    allUsers.map(user => {
+      user.name === userName
+        ? resolve({ ...user, active: !user.active })
+        : reject(user);
+    });
+  });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
