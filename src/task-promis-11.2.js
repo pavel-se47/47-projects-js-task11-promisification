3944ea1fb2cf0577ad4db074fd413496c773c01a
Pavel Hubarenka <pavel-se47@mail.ru>
2022-12-18 00:56:05 +0300

    add change vol2

--- src/task-promis-11.2.js
+++ src/task-promis-11.2.js
@@ -1,26 +1,3 @@
-// const users = [
-//   { name: 'Mango', active: true },
-//   { name: 'Poly', active: false },
-//   { name: 'Ajax', active: true },
-//   { name: 'Lux', active: false },
-// ];
-
-// const toggleUserState = (allUsers, userName, callback) => {
-//   const updatedUsers = allUsers.map(user =>
-//     user.name === userName ? { ...user, active: !user.active } : user
-//   );
-
-//   callback(updatedUsers);
-// };
-
-// const logger = updatedUsers => console.table(updatedUsers);
-
-// /*
-//  * Сейчас работает так
-//  */
-// toggleUserState(users, 'Mango', logger);
-// toggleUserState(users, 'Lux', logger);
-
 const users = [
   { name: 'Mango', active: true },
   { name: 'Poly', active: false },
@@ -45,3 +22,26 @@ const logger = updatedUsers => console.table(updatedUsers);
  */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
+
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user
+//   );
+
+//   callback(updatedUsers);
+// };
+
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// /*
+//  * Сейчас работает так
+//  */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
--- src/task-promis-11.3.js
+++ src/task-promis-11.3.js
@@ -35,7 +35,7 @@ makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 
 makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
-// makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
 
 // const randomIntegerFromInterval = (min, max) => {
 //   return Math.floor(Math.random() * (max - min + 1) + min);
