9427fb03a9f354ead56057d6d4e2818c83b2d46f
Pavel Hubarenka <pavel-se47@mail.ru>
2022-12-18 00:47:23 +0300

    add change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// import taskPromis from './task-promis-11.1';
-// import taskPromis from './task-promis-11.2';
-import taskPromis from './task-promis-11.3';
+import taskPromis1 from './task-promis-11.1';
+import taskPromis2 from './task-promis-11.2';
+import taskPromis3 from './task-promis-11.3';
